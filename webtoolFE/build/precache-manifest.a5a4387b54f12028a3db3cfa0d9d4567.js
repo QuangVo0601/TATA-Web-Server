4a9e233e23c15c5ad6770879deda09ec3b2ca040
vo Ryan <ryanvo161996@gmail.com>
2019-07-02 11:47:33 -0400

    Merged taskPage & groupingPage

---
+++ webtoolFE/build/precache-manifest.a5a4387b54f12028a3db3cfa0d9d4567.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ed9b801560fb66fb27b533eba2d14c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34642da25f106541cf6d",
+    "url": "/static/css/main.cd32c59b.chunk.css"
+  },
+  {
+    "revision": "1c79e459e2a523855af6",
+    "url": "/static/js/2.3bd0fa61.chunk.js"
+  },
+  {
+    "revision": "34642da25f106541cf6d",
+    "url": "/static/js/main.160e99c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a22539eb4f1047ae9f356d19e81c96d",
+    "url": "/static/media/Help Icon.7a22539e.png"
+  },
+  {
+    "revision": "7258d4022f0fc51d93d2fddac1916d6b",
+    "url": "/static/media/about.7258d402.png"
+  },
+  {
+    "revision": "b902a75a12f57dc8f5d865760cbaec72",
+    "url": "/static/media/home.b902a75a.png"
+  }
+]);
