05fe73505ee1edcc97d4fa076cf10ab6d8968236
vo Ryan <ryanvo161996@gmail.com>
2019-07-03 11:23:54 -0400

    small change to taskPage.js & tata.css

---
+++ webtoolFE/build/precache-manifest.9eee9aa2a0afeb7d0d9b3687b5973ac6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e71683fe1554909f31771a8ad9b6a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "565d5cf4f554c6871d73",
+    "url": "/static/css/main.8d5f2eac.chunk.css"
+  },
+  {
+    "revision": "a4228253d0354cada02b",
+    "url": "/static/js/2.1b804074.chunk.js"
+  },
+  {
+    "revision": "565d5cf4f554c6871d73",
+    "url": "/static/js/main.52c2b22a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a22539eb4f1047ae9f356d19e81c96d",
+    "url": "/static/media/Help Icon.7a22539e.png"
+  },
+  {
+    "revision": "7258d4022f0fc51d93d2fddac1916d6b",
+    "url": "/static/media/about.7258d402.png"
+  },
+  {
+    "revision": "b902a75a12f57dc8f5d865760cbaec72",
+    "url": "/static/media/home.b902a75a.png"
+  }
+]);
