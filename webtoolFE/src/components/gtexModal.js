abb347a92f32d8e0b26498fe28994cce5766bdb5
phuong_tran <phuongg.trann8799@gmail.com>
2019-07-11 10:35:18 -0400

    Update axios format

--- webtoolFE/src/components/gtexModal.js
+++ webtoolFE/src/components/gtexModal.js
@@ -1,6 +1,6 @@
 
 
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Multiselect from 'react-multiselect-checkboxes'
 import "../styles/tata.css";
 import axios from 'axios'; // to send data to back end
@@ -24,8 +24,8 @@ class GtexModule extends React.Component {
             // ---- //
 
             sexOpt: [
-                {label: "Male", value: "Male"},
-                {label: "Female", value:"Female"}
+                { label: "Male", value: "Male" },
+                { label: "Female", value: "Female" }
             ],
             sex: [],
             // ---- //
@@ -39,17 +39,17 @@ class GtexModule extends React.Component {
             ],
             death: [],
             // ---- //
-            
-            tissue1: ['Adipose Tissue','Adrenal Gland','Bladder','Brain','Breast','Cervix Uteri','Colon','Esophagus',
-                        'Fallopian Tube','Heart','Kidney','Liver','Lung','Muscle','Nerve'],
-            tissue2: ['Ovary','Pancreas','Pituitary','Prostate','Salivary Gland','Skin','Small Intestine','Spleen',
-                        'Stomach','Testis','Thyroid','Uterus','Vagina','Whole Blood'],
+
+            tissue1: ['Adipose Tissue', 'Adrenal Gland', 'Bladder', 'Brain', 'Breast', 'Cervix Uteri', 'Colon', 'Esophagus',
+                'Fallopian Tube', 'Heart', 'Kidney', 'Liver', 'Lung', 'Muscle', 'Nerve'],
+            tissue2: ['Ovary', 'Pancreas', 'Pituitary', 'Prostate', 'Salivary Gland', 'Skin', 'Small Intestine', 'Spleen',
+                'Stomach', 'Testis', 'Thyroid', 'Uterus', 'Vagina', 'Whole Blood'],
             tissueType: [],
-            tissueObj:{},
+            tissueObj: {},
         }
 
     }
-    componentDidMount(){
+    componentDidMount() {
         this.handleTissueState()
     }
 
@@ -58,74 +58,74 @@ class GtexModule extends React.Component {
     handleAgeOnChange = (selected) => {
         console.log(selected)
         let arr = []
-        selected.forEach(element=>{
+        selected.forEach(element => {
             arr.push(element.value)
         })
-        this.setState({ages:arr})
-  }
-  // Sex update handling
+        this.setState({ ages: arr })
+    }
+    // Sex update handling
     handleSexOnChange = (selected) => {
         let arr = []
-        for (let i=0; i<selected.length;i++) {
+        for (let i = 0; i < selected.length; i++) {
             arr.push(selected[i].value)
         }
-        this.setState({ sex : arr});
+        this.setState({ sex: arr });
     }
     // Death update handling
     handleDeathOnChange = (selected) => {
         let arr = []
-        for (let i=0; i<selected.length;i++) {
+        for (let i = 0; i < selected.length; i++) {
             arr.push(selected[i].value)
         }
-        this.setState({ death : arr});
+        this.setState({ death: arr });
     }
 
     /* For Tissue type handling */
-        handleTissueState() {
-        let obj=this.state.tissueObj
-        this.state.tissue1.map((item)=>{
-            obj[item]=false
+    handleTissueState() {
+        let obj = this.state.tissueObj
+        this.state.tissue1.map((item) => {
+            obj[item] = false
         })
-        this.state.tissue2.map((item)=>{
-            obj[item]=false
+        this.state.tissue2.map((item) => {
+            obj[item] = false
         })
-        this.setState({tissueObj:obj})
+        this.setState({ tissueObj: obj })
     }
 
-    handleTissueClick = (event,key) => {
+    handleTissueClick = (event, key) => {
         let o = this.state.tissueObj
         o[key] = !o[key]
-        if(o[key]){
+        if (o[key]) {
             event.target.classList.add('selectedLI')
         }
-        if(!o[key]){
+        if (!o[key]) {
             event.target.classList.remove('selectedLI')
         }
-        this.setState({tissueObj:o})
-        
+        this.setState({ tissueObj: o })
+
     }
 
     // toggle 'create'
-    createHandler=()=>{
-        let arr=[]
-        this.state.tissue1.forEach(item=>{
-            if(this.state.tissueObj[item]){
+    createHandler = () => {
+        let arr = []
+        this.state.tissue1.forEach(item => {
+            if (this.state.tissueObj[item]) {
                 arr.push(item)
             }
         })
-        this.state.tissue2.forEach(item=>{
-            if(this.state.tissueObj[item]){
+        this.state.tissue2.forEach(item => {
+            if (this.state.tissueObj[item]) {
                 arr.push(item)
             }
         })
-        this.setState({tissueType:arr}).then(()=>{
-            axios.post('http://127.0.0.1:8000/backend/detail', {
-                //axios.post('http://oscar19.orc.gmu.edu/backend/detail', {
-                    gtex: [this.state.sex,this.state.ages,this.state.death,this.state.tissueType]
-                })
+        this.setState({ tissueType: arr })
+        axios.post('http://127.0.0.1:8000/backend/detail', {
+            //axios.post('http://oscar19.orc.gmu.edu/backend/detail', {
+            gtex: [[], this.state.sex, this.state.ages, this.state.death, [], this.state.tissueType]
         })
+
     }
-                
+
 
     /* End of tissue type handling */
 
@@ -167,48 +167,48 @@ class GtexModule extends React.Component {
                                 <div id="age">
                                     <h2>Age Range:</h2></div> {/*<!--pick one or more, send to Math team, click refresh to update sample count-->*/}
                                 <div id="age_drop_down">
-                                    <Multiselect 
-                                    isMulti
-                                    value={this.state.age}
-                                    onChange={this.handleAgeOnChange}
-                                    closeMenuOnSelect={false}
-                                    options={this.state.ageOpt}
-                                />
+                                    <Multiselect
+                                        isMulti
+                                        value={this.state.age}
+                                        onChange={this.handleAgeOnChange}
+                                        closeMenuOnSelect={false}
+                                        options={this.state.ageOpt}
+                                    />
                                 </div>
-                                
+
                                 <div id="sex">
                                     <h2>Sex:</h2></div> {/*<!--pick one or more, send to Math team, click refresh to update sample count-->*/}
                                 <div className="sex-dropdown">
-                                <Multiselect
-                                    isMulti
-                                    onChange={this.handleSexOnChange}
-                                    closeMenuOnSelect={false}
-                                    options={this.state.sexOpt}
-                                />
+                                    <Multiselect
+                                        isMulti
+                                        onChange={this.handleSexOnChange}
+                                        closeMenuOnSelect={false}
+                                        options={this.state.sexOpt}
+                                    />
                                 </div>
 
                                 <div id="death">
                                     <h2>Death:</h2></div> {/*<!--pick one or more, send to Math team, click refresh to update sample count-->*/}
                                 <div className="death-dropdown">
-                                <Multiselect
-                                    isMulti
-                                    onChange={this.handleDeathOnChange}
-                                    closeMenuOnSelect={false}
-                                    options={this.state.deathOpt}
-                                />
+                                    <Multiselect
+                                        isMulti
+                                        onChange={this.handleDeathOnChange}
+                                        closeMenuOnSelect={false}
+                                        options={this.state.deathOpt}
+                                    />
 
                                 </div>
-        
+
                             </div>
                             <div id="title">
                                 <h2>Select Tissue type(s):</h2></div> {/*<!--pick one or more, send to Math team, click refresh to update sample count-->*/}
                             <div id="all">
                                 <div id="set1">
-                                    <ul> 
-                                        {this.state.tissue1.map((name)=><li onClick={(event)=>this.handleTissueClick(event,name)}>{name}</li>)}
+                                    <ul>
+                                        {this.state.tissue1.map((name) => <li onClick={(event) => this.handleTissueClick(event, name)}>{name}</li>)}
                                     </ul>
                                     <ul>
-                                    {this.state.tissue2.map((name)=><li onClick={(event)=>this.handleTissueClick(event,name)}>{name}</li>)}
+                                        {this.state.tissue2.map((name) => <li onClick={(event) => this.handleTissueClick(event, name)}>{name}</li>)}
                                     </ul>
                                 </div>
                             </div>
