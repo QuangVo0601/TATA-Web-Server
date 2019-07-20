5da0159d54d8a4fa018b0c7752c891e4f488a4c9
vo Ryan <ryanvo161996@gmail.com>
2019-07-20 19:38:26 -0400

    updated query.py & views.py

--- inputPage/views.py
+++ inputPage/views.py
@@ -6,7 +6,7 @@ from .serializers import InputSerializer
 from django.shortcuts import render
 from .ds_class import ds # import from ds_class.py for validationPage.js
 from .GTEXDataAnalyzer import GTEXDataAnalyzer # import from GTEXDataAnalyzer.py for batchPage.js
-from .query import get_sample_names # import from query.py for gtexModal.js
+from .query import get_sample_names, process_group_query # import from query.py for gtexModal.js
 
 # Create your views here.
 def index_page(request):
@@ -32,7 +32,7 @@ def input_list(request):
         for i in dataArray2:
             i[0] = i[0][:15] # get only first 15 characters of ENS IDs. Ex: ENSG00000223972.4 will become ENSG00000223972 (no decimal)
 
-        #dataArray2[0][0] = "ENS ID" # change the header to "ENS ID" to be used by ds_class.py
+        dataArray2[0][0] = "ENS ID" # change the header to "ENS ID" to be used by ds_class.py
         dataArray = [','.join(dataArray2[i]) for i in range(len(dataArray2))] # convert back to 1d array
         dataString = '\n'.join(dataArray) # convert the array back to string to be used in ds_class.py
 
@@ -79,6 +79,11 @@ def input_list2(request):
 
         # 'group_lists' is the list of lists of groups sent from groupingPage.js
         group_lists = data['groupsList'] # syntax: data[key], key = package's name being sent
+        # 'gtex_groups_queries' is the list of gtex group queries sent from groupingPage.js
+        #gtex_groups_queries = data['gtexQueries']
+
+        #print(group_lists)
+        #print(gtex_groups_queries)
         
         # Retrieve the 'dataString' saved in database
         dataString = request.session['dataString']
@@ -91,14 +96,40 @@ def input_list2(request):
         '''# dummy data to send to Math team, delete later
         group_lists = [['control group','#Patient1 (Gene Score)','#Patient3','#Patient5'],
                   ['group1','#Patient2','#Patient4'],
-                  ['group2','#Patient6','#Patient7'],
-                  ['gtex group','#Patient2','#Patient3','#Patient4']]'''
+                  ['group2','#Patient6','#Patient7']]'''
+
+        # what Jon wants:
+        # dont use Skin, Blood, Brain, Muscle for now (files are too big)  
+        gtex_groups_queries = [[[],[],['20-29', '30-39'],[],[], ['Kidney','Bladder'], 'gtex group 1'],
+		                    [[],['M'],['20-29', '30-39', '40-49', '50-59'],[],[], ['Liver', 'Kidney'], 'gtex group 2'],
+                            [[], ['F'], ['50-59'], ['Ventilator', 'Fast Natural'], [], ['Liver','Ovary', 'Salivary Gland',], 'gtex group 3']]
+
+        #           
+        #gtex_groups_dataframes = process_group_query(gtex_groups_queries)    
+
+        # python -w ignore
+
+        #print(gtex_groups_dataframes)     
+        # then append to CListDfs
 
         # pass in list of groups, return list of dataframes associated with list of groups
         CListDfs = dataSet.make_groups(group_lists) 
+        #print(CListDfs)
+
+        #CListDfs.extend(gtex_groups_dataframes)
+
+        #print(len(gtex_groups_dataframes))
+        #print(len(CListDfs))
+        #print(CListDfs)
 
         # get list of all group names (1st item in each list of group_lists)
         group_names_list = [group_lists[i][0] for i in range(len(group_lists))]
+        # get list of all gtex group names (7th item in each list of gtex_groups_queries)
+        #gtex_groups_names = [gtex_groups_queries[i][6] for i in range(len(gtex_groups_queries))]
+        # combine two lists above
+        #group_names_list.extend(gtex_groups_names)
+
+        #print(group_names_list)
 
         # Create an analyzer object from imported GTEXDataAnalyzer.py
         analyzer = GTEXDataAnalyzer(CListDfs, group_names_list) 
@@ -107,6 +138,7 @@ def input_list2(request):
         x_uncorrected_pca = [] # list of lists of x-axis of all dataframes
         y_uncorrected_pca = [] # list of lists of y-axis of all dataframes
         for df in analyzer.get_uncorrected_dataframes():
+            print(df)
             data_set_object = ds(5,df) 
             x, y = data_set_object.pca_graph() # call pca_graph func from ds_class.py
             x_uncorrected_pca.append(x)
@@ -147,19 +179,22 @@ def input_detail(request):
         print(gtexData)
         
         # call function from query.py to get sample count, 
-        # based on "gtexData" received from front end
-        # Ex:"gtexData" [[],['F'],['20-29', '30-39'],['Ventilator'],[],[ 'Skin','Blood']] = 192 samples
+        # based on "gtexData" received from front end . Ex: 'gtexData'
+        # [[],['F'],['20-29', '30-39'],['Ventilator'],[],['Skin','Blood']] = 192 samples
+        # [[], ['M'], ['70-79'], ['Ill Chronic'], [], []] = 38 samples
+        # [[], ['M', 'F'], ['50-59'], ['Ill Unexpected', 'Fast Natural'], [], ['Breast', 'Salivary Gland', 'Vagina']] = 43 samples
         for i in range(len(gtexData)):
             if (gtexData[i]): # if gtexData is not empty, call function
                 sample_names = get_sample_names(gtexData) 
                 sample_count = len(sample_names)
                 break
-            else: # if gtexData is empty [[], [], [], [], [], []], sample count will be max: 15598
+            else: # if gtexData is empty [[], [], [], [], [], []], sample count will be max: 11688
                 sample_names = [] # no sample names returned
-                sample_count = 15598 # max sample count
+                sample_count = 11688 # max sample count
         
         print(sample_names)
         print(sample_count)
+        
         return JsonResponse({'sample_count': sample_count,'sample_array': sample_names}, status=201) 
 
 '''@csrf_exempt
