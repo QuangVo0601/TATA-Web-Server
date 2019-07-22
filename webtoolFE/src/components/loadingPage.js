a58c88d256f59b1d7a8c391cf9529c608cf9bbe5
phuong_tran <phuongg.trann8799@gmail.com>
2019-07-22 11:19:34 -0400

    loading page merged

---
+++ webtoolFE/src/components/loadingPage.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import "../styles/loadingPage.css";
+import TopNav from './topNav'
+import BotNav from './botNav';
+
+class LoadingPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+    render() {
+        return (
+            <div className='loading-page'>
+                <head>
+                    <meta charset="UTF-8" />
+                    <title>jobCodePage</title>
+                    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700|Oswald:400,500&display=swap" rel="stylesheet" />
+                    <link href="jobCodePage.css" rel="stylesheet" type="text/css" />
+                </head>
+                <div id="wrapper">
+                    <TopNav />
+
+                    <div id="jobCode">JOB CODE:<span>1A253M4</span></div>
+                    <div id="etaInfo">
+                        <span className="percentage">47%</span>
+                    </div>
+                    <div className="loader">
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                        <span className="node"></span>
+                    </div>
+                    <div className="jobOptions">
+                        <div id="email">
+                            <div id="emailContent">
+                                Make sure to remember your job code or input your email for a reminder when the job is done.
+
+                                <input type="email" id="emailInput" name="emailAddress" placeholder="Email..." />
+                                <input type="submit" className="submitButton" />
+                            </div>
+                        </div>
+
+                        <div id="taskOptions">
+                            <div className="task_container">
+                                <button type="Continue" className="button job_new">Start New Task</button>
+                                <button type="Back" className="button job_cancel">Cancel This Task</button>
+                            </div>
+                        </div>
+
+                    </div>
+                     
+                </div>
+
+<BotNav />
+               
+            </div>
+        )
+    }
+}
+export default LoadingPage;
